4dc2deb136789bfba81cb6e05203d6ee2dcfb928
Khushnood Asif <72355837+khushnoodasif@users.noreply.github.com>
2021-09-05 21:29:04 +0100

    Add files via upload

---
+++ create-qr-code-with-python.py
@@ -0,0 +1,11 @@
+import pyqrcode
+from pyqrcode import QRCode
+
+#String that represent the QR Code
+s = "https://khushnoodasif.com"
+
+#Genreating QR Code
+qr = pyqrcode.create(s)
+
+#Saving QR Code in .svg format
+qr.svg("myyt.svg",scale = 7)
